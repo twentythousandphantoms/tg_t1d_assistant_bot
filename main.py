8da0d545aeb87b20ab788034d746086c5990b09a
Andrei.X.Belov.-ND <andrei.x.belov.-nd@disney.com>
2023-01-16 22:57:21 +0300

    Add: [main] add history options and keyboards

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import sqlite3
 import datetime
 import logging
 
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+
 from config import TOKEN
 
 # Set up logging
@@ -15,6 +17,43 @@ bot = telebot.TeleBot(TOKEN)
 
 def mg_dl_to_mmol_l(mg_dl):
     return round(mg_dl / 18.0182, 2)
+
+
+# button labels and callback data
+buttons = {
+    'entry_glucose_level': {'label': '🩸', 'callback_data': 'entry_glucose_level'},
+    'request_history': {'label': 'history', 'callback_data': 'request_history'}
+}
+
+history_options = {
+    '1_day': {'label': '1 day', 'callback_data': '1_day'},
+    '2_days': {'label': '2 days', 'callback_data': '2_days'},
+    'week': {'label': 'week', 'callback_data': 'week'},
+    'month': {'label': 'month', 'callback_data': 'month'},
+    'all': {'label': 'all', 'callback_data': 'all'}
+}
+
+# convert dictionaries to InlineKeyboardButton objects
+keyboard = [
+    [InlineKeyboardButton(buttons[key]['label'], callback_data=buttons[key]['callback_data']) for key in buttons]]
+history_keyboard = [
+    [InlineKeyboardButton(history_options[key]['label'], callback_data=history_options[key]['callback_data']) for key in
+     history_options]]
+
+
+# callback query handler
+@bot.callback_query_handler(func=lambda call: call.data in buttons)
+def process_callback_main(call):
+    if call.data == "entry_glucose_level":
+        bot.send_message(call.message.chat.id, "Please enter your glucose level")
+    elif call.data == "request_history":
+        bot.send_message(call.message.chat.id, "Please choose a history option",
+                         reply_markup=InlineKeyboardMarkup(history_keyboard))
+
+
+@bot.callback_query_handler(func=lambda call: call.data in history_options)
+def process_callback_history(call):
+    send_history_data(call.message.from_user.id, call.message.chat.id, call.data)
 
 
 @bot.message_handler(content_types=['text'])
@@ -41,31 +80,42 @@ def handle_message(message):
                          f'Your input of {user_input} has been saved, which is {mg_dl} mg/dl or {mmol_l} mmol/L')
         logger.info(f'User {user_id} sent {mg_dl} mg/dl ({mmol_l} mmol/L)')
         send_last_week_a1c(user_id, chat_id)
-    elif user_input == "history last day":
-        send_last_day_entries(user_id, chat_id)
+        # send message with inline keyboard
+        bot.send_message(chat_id, "Please choose an option", reply_markup=InlineKeyboardMarkup(keyboard))
     else:
-        bot.send_message(chat_id, "Invalid input. Please enter a number or 'history last day'")
+        bot.send_message(chat_id, "Invalid input. Please enter a number (glucose level) or choose an option",
+                         reply_markup=InlineKeyboardMarkup(keyboard))
 
 
-def send_last_day_entries(user_id, chat_id):
+def send_history_data(user_id: int, chat_id: int, period: str):
+    time_range = datetime.datetime.now() - datetime.timedelta(hours=12)
+    if period == '1_day':
+        time_range = datetime.datetime.now() - datetime.timedelta(days=1)
+    elif period == '2_days':
+        time_range = datetime.datetime.now() - datetime.timedelta(days=2)
+    elif period == 'week':
+        time_range = datetime.datetime.now() - datetime.timedelta(weeks=1)
+    elif period == 'month':
+        time_range = datetime.datetime.now() - datetime.timedelta(days=30)
+    elif period == 'all':
+        time_range = datetime.datetime(1970, 1, 1)
+
     conn = sqlite3.connect('user_inputs.db')
     c = conn.cursor()
-    c.execute(
-        "SELECT timestamp, user_id, mg_dl, mmol_l FROM user_inputs "
-        "WHERE user_id = ? and timestamp >= date('now', '-1 day') ORDER BY timestamp DESC",
-        (user_id,))
-    rows = c.fetchall()
-    if len(rows) == 0:
-        bot.send_message(chat_id, 'No entries for the last day')
+    c.execute("SELECT * FROM user_inputs WHERE timestamp > ? and user_id = ? ORDER BY timestamp DESC", (
+        time_range, user_id))
+    entries = c.fetchall()
+    if len(entries) == 0:
+        bot.send_message(chat_id, 'No entries for the last {}\n'.format(period))
     else:
-        entries = []
-        for row in rows:
-            timestamp = datetime.datetime.fromisoformat(row[0]).strftime("%d.%m %H:%M")
-            entry = f'{timestamp} - {row[2]} mg/dl or {row[3]} mmol/L'
-            entries.append(entry)
-        bot.send_message(chat_id, '\n'.join(entries))
-    c.close()
-    conn.close()
+        message = "Entries for the last {}:\n".format(period)
+        for entry in entries:
+            timestamp = entry[0].strftime("%d.%m %H:%M")
+            user_id = entry[1]
+            mg_dl = entry[2]
+            mmol_l = round(mg_dl / 18.0182, 1)
+            message += f"{timestamp} - {mg_dl} mg/dL ({mmol_l} mmol/L)\n"
+        bot.send_message(chat_id, message)
 
 
 def a1c_calculation(mg_dl):
